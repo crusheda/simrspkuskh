92b190e01d49b9ceb08d9f2c090c650c768643ec
Yussuf Faisal <thecrusheda@gmail.com>
2025-06-05 13:30:13 +0700

    showing catatan if verif null

--- resources/views/pages/klaim/detail.blade.php
+++ resources/views/pages/klaim/detail.blade.php
@@ -391,7 +391,64 @@
                     submit += `     </div>
                                 </div>`;
                     $('#footer_submit').empty().append(submit);
-                    $('#daftar_catatan').empty().append(`<div class="d-flex justify-content-center"><a class="align-middle">Tidak ada catatan</a></div>`);
+
+                    // REFRESH CATATAN
+                    $('#verif_accordion').prop('hidden',false);
+                    if (res.catatan.length != 0) {
+                        // console.log('sampai sini');
+                        $('#jumlah_catatan').text(res.catatan.length);
+                        $('#daftar_catatan').empty();
+                        res.catatan.forEach(item => {
+                            $('#daftar_catatan')
+                                .append(`<div class="col-md-12 border border-top-0 border-start-0 border-end-0 mb-3">
+                                            <div class="row">
+                                                <div class="col">
+                                                    <div class="">
+                                                        <h4 class="d-inline-block">${item.NAMAPEGAWAI} ${item.solved == 0?'<span class="badge text-bg-danger p-1">Belum Terselesaikan</span>':'<span class="badge text-bg-success p-1">Terselesaikan</span>'}</h4>
+                                                        <p class="text-muted">${moment(item.updated_at).locale('id').fromNow()} (${moment(item.updated_at).locale('id').format('D MMMM YYYY, [Pukul] HH:mm [WIB]')})</p>
+                                                    </div>
+                                                </div>
+                                                <div class="col-auto">
+                                                    <ul class="list-unstyled mb-0">
+                                                        ${item.solved == 0?
+                                                            `<li class="d-inline-block f-20 me-2">
+                                                                <a href="javascript: void(0);" data-bs-toggle="tooltip" title="Ubah Catatan" class="text-warning" onclick="ubahCatatan(${item.id})">
+                                                                    <i class="fas fa-edit"></i>
+                                                                </a>
+                                                            </li>
+                                                            <li class="d-inline-block f-20">
+                                                                <a href="javascript: void(0);" data-bs-toggle="tooltip" title="Hapus Catatan" class="text-danger" onclick="hapusCatatan(${item.id})">
+                                                                    <i class="fas fa-trash"></i>
+                                                                </a>
+                                                            </li>`
+                                                        :
+                                                            `<li class="d-inline-block f-20 me-2">
+                                                                <a href="javascript: void(0);" class="text-secondary">
+                                                                    <i class="fas fa-edit"></i>
+                                                                </a>
+                                                            </li>
+                                                            <li class="d-inline-block f-20">
+                                                                <a href="javascript: void(0);" class="text-secondary">
+                                                                    <i class="fas fa-trash"></i>
+                                                                </a>
+                                                            </li>`
+                                                        }
+
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <p>${item.deskripsi?item.deskripsi:'-'}</p>
+                                            </div>
+                                        </div>`);
+                        })
+                        // Showing Tooltip
+                        $('[data-bs-toggle="tooltip"]').tooltip({
+                            trigger : 'hover'
+                        })
+                    } else {
+                        $('#daftar_catatan').empty().append(`<div class="d-flex justify-content-center"><a class="align-middle">Tidak ada catatan</a></div>`);
+                    }
                 } else {
                     let koleksi = JSON.parse(res.show.koleksi || '[]');
                     if (koleksi.includes(1)) { $('#ck_sep').prop('checked', true).prop('disabled', false); } else { $('#ck_sep').prop('checked', false).prop('disabled',false); }
