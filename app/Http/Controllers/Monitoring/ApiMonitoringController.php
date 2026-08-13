260812b6a6c4ec6dc96413a052c4ae29da6c2197
Arizal <muhammadarizal33@gmail.com>
2026-08-13 11:02:46 +0700

    Fix ttd double resume sirmed

--- app/Http/Controllers/Monitoring/ApiMonitoringController.php
+++ app/Http/Controllers/Monitoring/ApiMonitoringController.php
@@ -1351,13 +1351,13 @@ class ApiMonitoringController extends Controller
                             if ($getTtd) {
                                 $imagePath2 = storage_path()."/app/public/".$getTtd->signature_path;
 
-                                DB::table('simrspku_klaim.tanda_tangan')->insert([
-                                    'kunjungan' => $kunjungan,
-                                    'signature_path' => $getTtd->signature_path,
-                                    'created_at' => Carbon::now(),
-                                    'updated_at' => Carbon::now(),
-                                    'user' => Auth::user()->ID,
-                                ]);
+                                // DB::table('simrspku_klaim.tanda_tangan')->insert([
+                                //     'kunjungan' => $kunjungan,
+                                //     'signature_path' => $getTtd->signature_path,
+                                //     'created_at' => Carbon::now(),
+                                //     'updated_at' => Carbon::now(),
+                                //     'user' => Auth::user()->ID,
+                                // ]);
                             } else {
                                 return response()->json([
                                     'message' => "Tanda tangan Dokter DPJP ({$item->NAMADOKTER}) pada Resume Konsul tidak ditemukan."
