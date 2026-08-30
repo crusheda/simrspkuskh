4955f878ae221cc100bd5c6eaeb816601a09d39a
Yussuf Faisal <thecrusheda@gmail.com>
2026-08-30 13:49:25 +0700

    fix

--- resources/views/pages/v2/medicalrecord/detail/form/pengkajian/bedahanestesi/praanestesiinduksi/index.blade.php
+++ resources/views/pages/v2/medicalrecord/detail/form/pengkajian/bedahanestesi/praanestesiinduksi/index.blade.php
@@ -1,8 +1,274 @@
 <div class="form-wrapper" id="form_khusus_nyerikronik">
     <h1 class="display-6 mb-1 mt-2 fs-23 fw-medium"><center>PENGKAJIAN <b class="text-success">PRA ANESTESI DAN INDUKSI</b></center></h1>
-    <div class="form-content">
-        <div class="row">
-            {{-- COL --}}
+    <div class="form-content mt-3">
+        <div class="row mb-4">
+            <div class="col-md-6">
+                <div class="d-flex align-items-center justify-content-between pe-md-3 mb-2">
+                    <label class="form-label mb-0 text-nowrap">
+                        Hilangnya Gigi
+                    </label>
+
+                    <div class="d-flex align-items-center gap-3">
+                        <div class="form-check mb-0">
+                            <input class="form-check-input single-checkbox" type="checkbox" name="pai_cb_">
+                            <label class="form-check-label">Tidak</label>
+                        </div>
+                        <div class="form-check mb-0">
+                            <input class="form-check-input single-checkbox" type="checkbox" name="pai_cb_">
+                            <label class="form-check-label">Ya</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center justify-content-between pe-md-3 mb-2">
+                    <label class="form-label mb-0 text-nowrap">
+                        Masalah mobilisasi leher
+                    </label>
+
+                    <div class="d-flex align-items-center gap-3">
+                        <div class="form-check mb-0">
+                            <input class="form-check-input single-checkbox" type="checkbox" name="pai_cb_">
+                            <label class="form-check-label">Tidak</label>
+                        </div>
+                        <div class="form-check mb-0">
+                            <input class="form-check-input single-checkbox" type="checkbox" name="pai_cb_">
+                            <label class="form-check-label">Ya</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center justify-content-between pe-md-3 mb-2">
+                    <label class="form-label mb-0 text-nowrap">
+                        Leher pendek
+                    </label>
+
+                    <div class="d-flex align-items-center gap-3">
+                        <div class="form-check mb-0">
+                            <input class="form-check-input single-checkbox" type="checkbox" name="pai_cb_">
+                            <label class="form-check-label">Tidak</label>
+                        </div>
+                        <div class="form-check mb-0">
+                            <input class="form-check-input single-checkbox" type="checkbox" name="pai_cb_">
+                            <label class="form-check-label">Ya</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center justify-content-between pe-md-3 mb-2">
+                    <label class="form-label mb-0 text-nowrap">
+                        Batuk
+                    </label>
+
+                    <div class="d-flex align-items-center gap-3">
+                        <div class="form-check mb-0">
+                            <input class="form-check-input single-checkbox" type="checkbox" name="pai_cb_">
+                            <label class="form-check-label">Tidak</label>
+                        </div>
+                        <div class="form-check mb-0">
+                            <input class="form-check-input single-checkbox" type="checkbox" name="pai_cb_">
+                            <label class="form-check-label">Ya</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center justify-content-between pe-md-3 mb-2">
+                    <label class="form-label mb-0 text-nowrap">
+                        Sesak nafas
+                    </label>
+
+                    <div class="d-flex align-items-center gap-3">
+                        <div class="form-check mb-0">
+                            <input class="form-check-input single-checkbox" type="checkbox" name="pai_cb_">
+                            <label class="form-check-label">Tidak</label>
+                        </div>
+                        <div class="form-check mb-0">
+                            <input class="form-check-input single-checkbox" type="checkbox" name="pai_cb_">
+                            <label class="form-check-label">Ya</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center justify-content-between pe-md-3 mb-2">
+                    <label class="form-label mb-0 text-nowrap">
+                        Baru saja menderita inteksi saluran nafas atas
+                    </label>
+
+                    <div class="d-flex align-items-center gap-3">
+                        <div class="form-check mb-0">
+                            <input class="form-check-input single-checkbox" type="checkbox" name="pai_cb_">
+                            <label class="form-check-label">Tidak</label>
+                        </div>
+                        <div class="form-check mb-0">
+                            <input class="form-check-input single-checkbox" type="checkbox" name="pai_cb_">
+                            <label class="form-check-label">Ya</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center justify-content-between pe-md-3 mb-2">
+                    <label class="form-label mb-0 text-nowrap">
+                        Periode menstruasi tidak normal
+                    </label>
+
+                    <div class="d-flex align-items-center gap-3">
+                        <div class="form-check mb-0">
+                            <input class="form-check-input single-checkbox" type="checkbox" name="pai_cb_">
+                            <label class="form-check-label">Tidak</label>
+                        </div>
+                        <div class="form-check mb-0">
+                            <input class="form-check-input single-checkbox" type="checkbox" name="pai_cb_">
+                            <label class="form-check-label">Ya</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center justify-content-between pe-md-3 mb-2">
+                    <label class="form-label mb-0 text-nowrap">
+                        Stroke
+                    </label>
+
+                    <div class="d-flex align-items-center gap-3">
+                        <div class="form-check mb-0">
+                            <input class="form-check-input single-checkbox" type="checkbox" name="pai_cb_">
+                            <label class="form-check-label">Tidak</label>
+                        </div>
+                        <div class="form-check mb-0">
+                            <input class="form-check-input single-checkbox" type="checkbox" name="pai_cb_">
+                            <label class="form-check-label">Ya</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {{-- DIVIDER --}}
+            <div class="col-md-6 border-start">
+                <div class="d-flex align-items-center justify-content-between ps-md-3 mb-2">
+                    <label class="form-label mb-0 text-nowrap">
+                        Sakit Dada
+                    </label>
+
+                    <div class="d-flex align-items-center gap-3">
+                        <div class="form-check mb-0">
+                            <input class="form-check-input single-checkbox" type="checkbox" name="pai_cb_">
+                            <label class="form-check-label">Tidak</label>
+                        </div>
+                        <div class="form-check mb-0">
+                            <input class="form-check-input single-checkbox" type="checkbox" name="pai_cb_">
+                            <label class="form-check-label">Ya</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center justify-content-between ps-md-3 mb-2">
+                    <label class="form-label mb-0 text-nowrap">
+                        Denyut jantung tidak normal
+                    </label>
+
+                    <div class="d-flex align-items-center gap-3">
+                        <div class="form-check mb-0">
+                            <input class="form-check-input single-checkbox" type="checkbox" name="pai_cb_">
+                            <label class="form-check-label">Tidak</label>
+                        </div>
+                        <div class="form-check mb-0">
+                            <input class="form-check-input single-checkbox" type="checkbox" name="pai_cb_">
+                            <label class="form-check-label">Ya</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center justify-content-between ps-md-3 mb-2">
+                    <label class="form-label mb-0 text-nowrap">
+                        Muntah
+                    </label>
+
+                    <div class="d-flex align-items-center gap-3">
+                        <div class="form-check mb-0">
+                            <input class="form-check-input single-checkbox" type="checkbox" name="pai_cb_">
+                            <label class="form-check-label">Tidak</label>
+                        </div>
+                        <div class="form-check mb-0">
+                            <input class="form-check-input single-checkbox" type="checkbox" name="pai_cb_">
+                            <label class="form-check-label">Ya</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center justify-content-between ps-md-3 mb-2">
+                    <label class="form-label mb-0 text-nowrap">
+                        Susah kencing
+                    </label>
+
+                    <div class="d-flex align-items-center gap-3">
+                        <div class="form-check mb-0">
+                            <input class="form-check-input single-checkbox" type="checkbox" name="pai_cb_">
+                            <label class="form-check-label">Tidak</label>
+                        </div>
+                        <div class="form-check mb-0">
+                            <input class="form-check-input single-checkbox" type="checkbox" name="pai_cb_">
+                            <label class="form-check-label">Ya</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center justify-content-between ps-md-3 mb-2">
+                    <label class="form-label mb-0 text-nowrap">
+                        Kejang
+                    </label>
+
+                    <div class="d-flex align-items-center gap-3">
+                        <div class="form-check mb-0">
+                            <input class="form-check-input single-checkbox" type="checkbox" name="pai_cb_">
+                            <label class="form-check-label">Tidak</label>
+                        </div>
+                        <div class="form-check mb-0">
+                            <input class="form-check-input single-checkbox" type="checkbox" name="pai_cb_">
+                            <label class="form-check-label">Ya</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center justify-content-between ps-md-3 mb-2">
+                    <label class="form-label mb-0 text-nowrap">
+                        Sedang hamil
+                    </label>
+
+                    <div class="d-flex align-items-center gap-3">
+                        <div class="form-check mb-0">
+                            <input class="form-check-input single-checkbox" type="checkbox" name="pai_cb_">
+                            <label class="form-check-label">Tidak</label>
+                        </div>
+                        <div class="form-check mb-0">
+                            <input class="form-check-input single-checkbox" type="checkbox" name="pai_cb_">
+                            <label class="form-check-label">Ya</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center justify-content-between ps-md-3 mb-2">
+                    <label class="form-label mb-0 text-nowrap">
+                        Pingsan
+                    </label>
+
+                    <div class="d-flex align-items-center gap-3">
+                        <div class="form-check mb-0">
+                            <input class="form-check-input single-checkbox" type="checkbox" name="pai_cb_">
+                            <label class="form-check-label">Tidak</label>
+                        </div>
+                        <div class="form-check mb-0">
+                            <input class="form-check-input single-checkbox" type="checkbox" name="pai_cb_">
+                            <label class="form-check-label">Ya</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center justify-content-between ps-md-3 mb-2">
+                    <label class="form-label mb-0 text-nowrap">
+                        Obesitas
+                    </label>
+
+                    <div class="d-flex align-items-center gap-3">
+                        <div class="form-check mb-0">
+                            <input class="form-check-input single-checkbox" type="checkbox" name="pai_cb_">
+                            <label class="form-check-label">Tidak</label>
+                        </div>
+                        <div class="form-check mb-0">
+                            <input class="form-check-input single-checkbox" type="checkbox" name="pai_cb_">
+                            <label class="form-check-label">Ya</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="form-group mb-3">
+                    <h6 class="mb-2">Pemeriksaan Fisik</h6>
+                    <textarea class="form-control" name="pai_cb_pf" id="" rows="4"></textarea>
+                </div>
+            </div>
         </div>
     </div>
 </div>
--- resources/views/pages/v2/medicalrecord/detail/form/pengkajian/bedahanestesi/prabedah/index.blade.php
+++ resources/views/pages/v2/medicalrecord/detail/form/pengkajian/bedahanestesi/prabedah/index.blade.php
@@ -1,8 +1,147 @@
-<div class="form-wrapper" id="form_khusus_nyerikronik">
+<div class="form-wrapper" id="form_pra_bedah">
     <h1 class="display-6 mb-1 mt-2 fs-23 fw-medium"><center>PENGKAJIAN <b class="text-success">PRA BEDAH</b></center></h1>
-    <div class="form-content">
-        <div class="row">
-            {{-- COL --}}
+    <div class="form-content mt-3">
+        <div class="row mb-4">
+            <div class="col-md-12">
+                <div class="form-group mb-3">
+                    <h6 class="mb-2">Data Subyektif (Anamnesis)</h6>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="d-flex align-items-center gap-2 mb-2">
+                                <div class="form-check mb-0"><input class="form-check-input check-primary" type="checkbox" name="pb_ds_batuk" id=""></div>
+                                <label class="form-label mb-0" for="">Batuk</label>
+                            </div>
+                            <div class="d-flex align-items-center gap-2 mb-2">
+                                <div class="form-check mb-0"><input class="form-check-input check-primary" type="checkbox" name="pb_ds_pusing" id=""></div>
+                                <label class="form-label mb-0" for="">Pusing</label>
+                            </div>
+                            <div class="d-flex align-items-center gap-2 mb-2">
+                                <div class="form-check mb-0"><input class="form-check-input check-primary" type="checkbox" name="pb_ds_sesaknafas" id=""></div>
+                                <label class="form-label mb-0" for="">Sesak Nafas</label>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="d-flex align-items-center gap-2 mb-2">
+                                <div class="form-check mb-0"><input class="form-check-input check-primary" type="checkbox" name="pb_ds_pilek" id=""></div>
+                                <label class="form-label mb-0" for="">Pilek</label>
+                            </div>
+                            <div class="d-flex align-items-center gap-2 mb-2">
+                                <div class="form-check mb-0"><input class="form-check-input check-primary" type="checkbox" name="pb_ds_mual" id=""></div>
+                                <label class="form-label mb-0" for="">Mual</label>
+                            </div>
+                            <div class="d-flex align-items-center gap-2 mb-2">
+                                <div class="form-check mb-0"><input class="form-check-input check-primary" type="checkbox" name="pb_ds_puasa" id=""></div>
+                                <label class="form-label mb-0" for="">Puasa</label>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="d-flex align-items-center gap-2 mb-2">
+                                <div class="form-check mb-0"><input class="form-check-input check-primary" type="checkbox" name="pb_ds_gigipalsu" id=""></div>
+                                <label class="form-label mb-0" for="">Gigi Palsu</label>
+                            </div>
+                            <div class="d-flex gap-2 align-items-center">
+                                <div class="form-check mb-0">
+                                    <input class="form-check-input check-primary" type="checkbox" name="pb_ds_lain" id="">
+                                </div>
+                                <label class="form-label mb-0 flex-shrink-0">
+                                    Lainnya ,
+                                </label>
+                                <input type="text" class="form-control form-control-sm" data-enable-when="ds_lain" name="pb_ds_lain_sebut" placeholder="Sebutkan ..." disabled>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group mb-3">
+                    <h6 class="mb-2">Riwayat Operasi Sebelumnya</h6>
+                    <textarea class="form-control" name="pb_ros" id="" rows="2"></textarea>
+                </div>
+                <div class="form-group mb-3">
+                    <h6 class="mb-2">Data Obyektif (Pemeriksaan Fisik)</h6>
+                    <textarea class="form-control" name="pb_do" id="" rows="4"></textarea>
+                </div>
+                <div class="form-group mb-3">
+                    <h6 class="mb-2">Riwayat Penyakit</h6>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="d-flex align-items-center gap-2 mb-2">
+                                <div class="form-check mb-0"><input class="form-check-input check-primary" type="checkbox" name="pb_rp_dm" id=""></div>
+                                <label class="form-label mb-0" for="">DM</label>
+                            </div>
+                            <div class="d-flex align-items-center gap-2 mb-2">
+                                <div class="form-check mb-0"><input class="form-check-input check-primary" type="checkbox" name="pb_rp_tb" id=""></div>
+                                <label class="form-label mb-0" for="">TB Paru</label>
+                            </div>
+                            <div class="d-flex align-items-center gap-2 mb-2">
+                                <div class="form-check mb-0"><input class="form-check-input check-primary" type="checkbox" name="pb_rp_hepatitis" id=""></div>
+                                <label class="form-label mb-0" for="">Hepatitis B-C</label>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="d-flex align-items-center gap-2 mb-2">
+                                <div class="form-check mb-0"><input class="form-check-input check-primary" type="checkbox" name="pb_rp_hipertensi" id=""></div>
+                                <label class="form-label mb-0" for="">Hipertensi</label>
+                            </div>
+                            <div class="d-flex align-items-center gap-2 mb-2">
+                                <div class="form-check mb-0"><input class="form-check-input check-primary" type="checkbox" name="pb_rp_stemi" id=""></div>
+                                <label class="form-label mb-0" for="">STEMI / ACS</label>
+                            </div>
+                            <div class="d-flex align-items-center gap-2 mb-2">
+                                <div class="form-check mb-0"><input class="form-check-input check-primary" type="checkbox" name="pb_rp_hiv" id=""></div>
+                                <label class="form-label mb-0" for="">HIV / AIDS</label>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="d-flex align-items-center gap-2 mb-2">
+                                <div class="form-check mb-0"><input class="form-check-input check-primary" type="checkbox" name="pb_rp_asma" id=""></div>
+                                <label class="form-label mb-0" for="">Asma</label>
+                            </div>
+                            <div class="d-flex align-items-center gap-2 mb-2">
+                                <div class="form-check mb-0"><input class="form-check-input check-primary" type="checkbox" name="pb_rp_chf" id=""></div>
+                                <label class="form-label mb-0" for="">CHF</label>
+                            </div>
+                            <div class="d-flex gap-2 align-items-center">
+                                <div class="form-check mb-0">
+                                    <input class="form-check-input check-primary" type="checkbox" name="pb_rp_lain" id="">
+                                </div>
+                                <label class="form-label mb-0 flex-shrink-0">
+                                    Lainnya ,
+                                </label>
+                                <input type="text" class="form-control form-control-sm" data-enable-when="ds_lain" name="pb_rp_lain_sebut" placeholder="Sebutkan ..." disabled>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="mb-3">
+                    @include('pages.v2.medicalrecord.detail.form.pengkajian.components.pemeriksaan_lab')
+                </div>
+                <div class="mb-3">
+                    @include('pages.v2.medicalrecord.detail.form.pengkajian.components.pemeriksaan_rad')
+                </div>
+                <div class="form-group mb-3">
+                    <h6 class="mb-2">Diagnosa Pra Operasi</h6>
+                    <textarea class="form-control" name="pb_dpo" id="" rows="2"></textarea>
+                </div>
+                <div class="form-group mb-3">
+                    <h6 class="mb-2">Posisi Pasien Dalam Operasi</h6>
+                    <textarea class="form-control" name="pb_ppdo" id="" rows="2"></textarea>
+                </div>
+                <div class="form-group mb-3">
+                    <h6 class="mb-2">Profilaksis</h6>
+                    <textarea class="form-control" name="pb_pro" id="" rows="2"></textarea>
+                </div>
+                <div class="form-group mb-3">
+                    <h6 class="mb-2">Perkiraan Lama Operasi</h6>
+                    <textarea class="form-control" name="pb_plo" id="" rows="2"></textarea>
+                </div>
+                <div class="form-group mb-3">
+                    <h6 class="mb-2">Alat Khusus</h6>
+                    <textarea class="form-control" name="pb_ak" id="" rows="2"></textarea>
+                </div>
+                <div class="form-group">
+                    <h6 class="mb-2">Rencana Operasi</h6>
+                    <textarea class="form-control" name="pb_ro" id="" rows="4"></textarea>
+                </div>
+            </div>
         </div>
     </div>
 </div>
@@ -11,7 +150,7 @@
 (function () {
     'use strict';
 
-    const $form = $('#form_khusus_nyerikronik');
+    const $form = $('#form_pra_bedah');
 
     let isDataLoading = false;
     let isDataSaving = false;
@@ -29,7 +168,7 @@
         isDataLoading = true;
 
         $.ajax({
-            url: `/api/v2/emr/form/pengkajian/khu/nyerikronik/${kunjungan}`,
+            url: `/api/v2/emr/form/pengkajian/bedans/prabedah/${kunjungan}`,
             type: 'GET',
             dataType: 'json',
 
@@ -93,7 +232,7 @@
         isDataSaving = true;
 
         $.ajax({
-            url: `/api/v2/emr/form/pengkajian/khu/nyerikronik/${kunjungan}/simpan`,
+            url: `/api/v2/emr/form/pengkajian/bedans/prabedah/${kunjungan}/simpan`,
             type: 'POST',
             data: data,
 
