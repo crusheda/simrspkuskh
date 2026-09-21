8017b5e80b8d0eabbe97e1144ddfff347eff513f
Yussuf Faisal <thecrusheda@gmail.com>
2026-09-21 14:02:01 +0700

    fix bug finalisasi ranap anak

--- resources/views/pages/v2/medicalrecord/detail/form/pengkajian/rawat-inap/anak/form_dokter.blade.php
+++ resources/views/pages/v2/medicalrecord/detail/form/pengkajian/rawat-inap/anak/form_dokter.blade.php
@@ -1,4 +1,4 @@
-<div class="form-wrapper">
+<div class="form-wrapper" id="form_ranap_anak_dokter">
     <h1 class="display-6 mb-1 fs-27 fw-bold"><center>PENGKAJIAN MEDIS <b class="text-danger">RAWAT INAP</b> <b class="text-warning">ANAK</b></center></h1>
     <h1 class="display-6 mb-4 fs-18"><center>(<a class="text-danger">Diisi Oleh Dokter</a>)</center></h1>
     <div class="form-content">
